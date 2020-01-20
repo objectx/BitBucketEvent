6ae2892d6e3cedaab83d27c8a7ff868448292296
Masashi Fujita <objectxtreme@gmail.com>
2020-01-21 00:02:43 +0900

    refactor: Remove redundant `new`

--- src/BitBucketEvent/Types/CommitHash.fs
+++ src/BitBucketEvent/Types/CommitHash.fs
@@ -9,7 +9,7 @@ open System.Text
 type CommitHash = byte array
 
 let toString (h: CommitHash): string =
-    let sb = new StringBuilder(2 * h.Length)
+    let sb = StringBuilder(2 * h.Length)
     for v in h do
         sb.AppendFormat("{0:x2}", v) |> ignore
     sb.ToString()
@@ -45,7 +45,7 @@ let fromString (s: string): CommitHash =
         (u <<< 4) ||| (l <<< 0)
     let len = s.Length
     if (len % 2) <> 0 then failwithf "odd length (%d)" len
-    let result = new ResizeArray<byte>(s.Length / 2)
+    let result = ResizeArray<byte>(s.Length / 2)
     for i in 0 .. 2 .. (s.Length - 2) do
         combine (s.[i + 0] |> toHex) (s.[i + 1] |> toHex) |> result.Add
     result.ToArray()
